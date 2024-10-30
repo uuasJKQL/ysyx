61ffc3dd06534ae1ba3f467a4e9c87adc960b0b4
tracer-ysyx <tracer@ysyx.org>
2024-10-30 21:17:35 +0800

    > compile NEMU ysyx_22040000 张三 Linux wyh-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:17:35 up  1:50,  1 user,  load average: 0.78, 0.59, 0.62

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -16,7 +16,8 @@
 #include <isa.h>
 #include "local-include/reg.h"
 
-const char *regs[] = {
+const char *regs[] = 
+{
   "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
   "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
   "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
@@ -25,9 +26,12 @@ const char *regs[] = {
 
 void isa_reg_display() {
 //printf("11");
+for(int i=0;i<32;i++)
+{
+printf("reg%s:0x%x\n",regs[i],cpu.gpr[2]);
+//printf("reg:0x%x",cpu.pc);
 
-printf("reg:0x%x",cpu.gpr[2]);
-printf("reg:0x%x",cpu.pc);
+}
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
