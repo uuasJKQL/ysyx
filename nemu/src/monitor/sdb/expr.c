ffcf01a379afb753a0a2c27b48c09a4576e1f92b
tracer-ysyx <tracer@ysyx.org>
2025-03-24 20:40:38 +0800

    > compile NEMU ysyx_22040000 王宇辉 Linux wyh-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:40:38 up 38 min,  1 user,  load average: 1.16, 0.92, 0.87

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -43,7 +43,7 @@ static struct rule {
   {"&&",TK_AND},
   {"[0-9]+",TK_number},
   {"\\$[A-Za-z][0-9]+",TK_REG},
-  {"[0-9a-fA-F]+",TK_HEX},
+  {"x[0-9a-fA-F]+",TK_HEX},
   {"\\(",'('},
   {"\\)",')'},
   {"-",'-'},
