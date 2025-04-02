e3e91cbc1ab402c6961a1ee981e8ca12e69abcc4
tracer-ysyx <tracer@ysyx.org>
2025-04-02 22:29:16 +0800

    > sim RTL ysyx_22040000 王宇辉 Linux wyh-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:29:16 up 36 min,  1 user,  load average: 0.10, 0.22, 0.49

--- npc/obj_dir/Vtop.mk
+++ npc/obj_dir/Vtop.mk
@@ -57,7 +57,7 @@ include $(VERILATOR_ROOT)/include/verilated.mk
 ### Executable rules... (from --exe)
 VPATH += $(VM_USER_DIR)
 
-testbench.o: testbench.c
+testbench.o: testbench.cpp
 	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST) -c -o $@ $<
 
 ### Link rules... (from --exe)
