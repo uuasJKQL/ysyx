47114bdcc0a3d132d6711f976eb7f61474fefbe1
tracer-ysyx <tracer@ysyx.org>
2025-04-08 16:37:20 +0800

    > sim RTL ysyx_22040000 王宇辉 Linux wyh-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:37:20 up 42 min,  1 user,  load average: 0.29, 0.41, 0.49

--- npc/testbench.cpp
+++ npc/testbench.cpp
@@ -108,7 +108,7 @@ int main(int argc, char **argv)
         {
             dut->clk = !dut->clk;
             dut->eval();
-            tfp->dump(100 * rep + i * 10 + 100);
+            tfp->dump(330 * rep + i * 10 + 1000);
         }
     }
 
