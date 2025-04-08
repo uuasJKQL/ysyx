3dc7da9f0a4c0952f102ee46c64d69b9a5100c87
tracer-ysyx <tracer@ysyx.org>
2025-04-08 16:24:48 +0800

    > sim RTL ysyx_22040000 王宇辉 Linux wyh-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:24:48 up 30 min,  1 user,  load average: 0.56, 0.54, 0.56

--- npc/testbench.cpp
+++ npc/testbench.cpp
@@ -3,13 +3,14 @@
 #include <verilated_vcd_c.h>
 #include "Vtop.h"
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
     Verilated::commandArgs(argc, argv);
-    Vtop* dut = new Vtop;
+    Vtop *dut = new Vtop;
 
     // 波形初始化
     Verilated::traceEverOn(true);
-    VerilatedVcdC* tfp = new VerilatedVcdC;
+    VerilatedVcdC *tfp = new VerilatedVcdC;
     dut->trace(tfp, 99);
     tfp->open("wave.vcd");
 
@@ -19,10 +20,11 @@ int main(int argc, char** argv) {
     dut->clrn = 0;
     dut->ps2_clk = 1;
     dut->ps2_data = 1;
-    dut->nextdata = 1;
+    //  dut->nextdata = 1;
 
     // 复位序列 (10个时钟周期)
-    for (int i = 0; i < 10; ++i) {
+    for (int i = 0; i < 10; ++i)
+    {
         dut->clk = !dut->clk;
         dut->eval();
         tfp->dump(time);
@@ -33,22 +35,34 @@ int main(int argc, char** argv) {
     // 模拟发送Q键扫描码
     const uint8_t make_code = 0x15;
     const uint8_t break_prefix = 0xF0;
-    
+
     // 发送按键按下
     // 发送BREAK前缀
     dut->ps2_data = 0; // 起始位
-    tfp->dump(time); time += 5;
-    dut->ps2_clk = 0; tfp->dump(time); time += 5;
-    dut->ps2_clk = 1; tfp->dump(time); time += 5;
-    
+    tfp->dump(time);
+    time += 5;
+    dut->ps2_clk = 0;
+    tfp->dump(time);
+    time += 5;
+    dut->ps2_clk = 1;
+    tfp->dump(time);
+    time += 5;
+
     // 数据位（BREAK_PREFIX 0xF0）
     uint8_t data = break_prefix;
-    for (int i = 0; i < 8; ++i) {
+    for (int i = 0; i < 8; ++i)
+    {
         dut->ps2_data = (data >> i) & 1;
-        dut->ps2_clk = 0; tfp->dump(time); time += 5;
-        dut->ps2_clk = 1; tfp->dump(time); time += 5;
+        dut->ps2_clk = 0;
+        dut->eval();
+        tfp->dump(time);
+        time += 5;
+        dut->ps2_clk = 1;
+        dut->eval();
+        tfp->dump(time);
+        time += 5;
     }
-    
+
     // ... 类似地完成完整的数据传输 ...
 
     // 清理
