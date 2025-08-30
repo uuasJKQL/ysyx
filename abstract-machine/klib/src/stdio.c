9858ce47b235601644cf6f1d8f263e302860c0c3
wyh <1494930903@qq.com>
2025-08-30 20:45:38 +0800

    pa 2.2 am

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -4,24 +4,116 @@
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
+void int_to_str(int num, char *str)
+{
 
-int printf(const char *fmt, ...) {
+  int i = 0;
+  int is_negative = 0;
+
+  // 处理负数
+  if (num < 0)
+  {
+    is_negative = 1;
+    num = -num;
+  }
+
+  // 处理0的特殊情况
+  if (num == 0)
+  {
+    str[i++] = '0';
+  }
+  else
+  {
+    // 将数字转换为字符串（反向）
+    while (num != 0)
+    {
+      str[i++] = '0' + (num % 10);
+      num /= 10;
+    }
+
+    // 添加负号（如果需要）
+    if (is_negative)
+    {
+      str[i++] = '-';
+    }
+
+    // 反转字符串
+    for (int j = 0; j < i / 2; j++)
+    {
+      char temp = str[j];
+      str[j] = str[i - 1 - j];
+      str[i - 1 - j] = temp;
+    }
+  }
+
+  // 添加字符串结束符
+  str[i] = '\0';
+}
+int printf(const char *fmt, ...)
+{
   panic("Not implemented");
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
+int vsprintf(char *out, const char *fmt, va_list ap)
+{
   panic("Not implemented");
 }
 
-int sprintf(char *out, const char *fmt, ...) {
+int sprintf(char *out, const char *fmt, ...)
+{
+  va_list ap;
+  va_start(ap, fmt);
+  char *s;
+  char *st = out;
+  int d;
+  char *s1 = "0";
+  while (*fmt) // 遍历格式字符串
+  {
+    switch (*fmt)
+    {
+    case '%': // 处理字符串参数
+
+      switch (*(++fmt))
+      {
+      case 's':
+        s = va_arg(ap, char *); // 获取char*类型的参数
+        strcpy(st, s);
+        st += strlen(s);
+        break;
+      case 'd':
+        d = va_arg(ap, int);
+        itoa(d, s1, 10);
+        strcpy(st, s1);
+        st += strlen(s1);
+        break;
+      case '%':
+        *st = *fmt;
+        st++;
+        break;
+      default:
+        break;
+      }
+      break;
+    default:
+
+      *st = *fmt;
+      st++;
+      break;
+    }
+    fmt++;
+  }
+  *st = '\0';
+  va_end(ap); // 清理工作
+  return 1;
+}
+
+int snprintf(char *out, size_t n, const char *fmt, ...)
+{
   panic("Not implemented");
 }
 
-int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
-}
-
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
+int vsnprintf(char *out, size_t n, const char *fmt, va_list ap)
+{
   panic("Not implemented");
 }
 
--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -4,44 +4,105 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-size_t strlen(const char *s) {
+size_t strlen(const char *s)
+{
+  size_t i = 0;
+  while (s[i])
+  {
+    i++;
+  }
+  return i;
+}
+
+char *strcpy(char *dst, const char *src)
+{
+  int i = 0;
+
+  while (src[i])
+  {
+    dst[i] = src[i];
+    i++;
+  }
+  dst[i] = '\0';
+  return dst;
+}
+
+char *strncpy(char *dst, const char *src, size_t n)
+{
   panic("Not implemented");
 }
 
-char *strcpy(char *dst, const char *src) {
+char *strcat(char *dst, const char *src)
+{
+  int i = 0;
+  int p = strlen(dst);
+  while (src[i])
+  {
+    dst[p + i] = src[i];
+    i++;
+  }
+  dst[p + i] = '\0';
+  return dst;
+}
+
+int strcmp(const char *s1, const char *s2)
+{
+  int i = 0;
+  while (1)
+  {
+    if ((s1[i] - s2[i]) != 0)
+      return s1[i] - s2[i];
+    else if (s1[i] == 0 && s2[i] == 0)
+      return 0;
+    i++;
+  }
+
+  return 0;
+}
+
+int strncmp(const char *s1, const char *s2, size_t n)
+{
   panic("Not implemented");
 }
 
-char *strncpy(char *dst, const char *src, size_t n) {
+void *memset(void *s, int c, size_t n)
+{
+  unsigned char *p = s;
+
+  for (int i = 0; i < n; i++)
+  {
+    *(p + i) = c;
+  }
+  return s;
+}
+
+void *memmove(void *dst, const void *src, size_t n)
+{
   panic("Not implemented");
 }
 
-char *strcat(char *dst, const char *src) {
+void *memcpy(void *out, const void *in, size_t n)
+{
   panic("Not implemented");
 }
 
-int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
-}
+int memcmp(const void *s1, const void *s2, size_t n)
+{
+  const unsigned char *p1 = s1;
+  const unsigned char *p2 = s2;
 
-int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
-}
-
-void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
-}
-
-void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
-}
-
-void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
-}
-
-int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  for (int i = 0; i < n; i++)
+  {
+    if (*(p1 + i) < *(p2 + i))
+    {
+      return -1;
+    }
+    else if (*(p1 + i) > *(p2 + i))
+    {
+      return 1;
+    }
+  }
+  return 0;
 }
 
 #endif
